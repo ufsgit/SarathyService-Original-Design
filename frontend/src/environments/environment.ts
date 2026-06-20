144b006ec293de221811a0fb06266795420620c6
swaroopufs <swaroop@ufstechnologies.in>
2026-06-20 12:51:01 +0530

    api link

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,5 +2,8 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000/api',
   // apiUrl: 'https://sarathyserviceapi.trackbox.live/api',
+  // apiUrl: 'ktmserviceapi.sarathymotors.in',
+  // apiUrl: 'bajajserviceapi.sarathymotors.in',
+
   FilePath: 'http://localhost:3000',
 };
