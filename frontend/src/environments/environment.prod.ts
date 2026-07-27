c3ea014ce1b82352dee4dda742c7e518f4eb2ac4
swaroopufs <swaroop@ufstechnologies.in>
2026-07-27 23:04:09 +0530

    enviroments updated

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,9 +3,15 @@ export const environment = {
 
   apiUrl: 'http://localhost:3000/api',
   FilePath: 'http://localhost:3000',
+
+  // apiUrl: 'https://stage-bajajserviceapi.sarathymotors.in/api',
+  // FilePath: 'http://stage-bajajserviceapi.sarathymotors.in',
+  
+  // apiUrl: 'https://stage-ktmserviceapi.sarathymotors.in/api',
+  // FilePath: 'https://stage-ktmserviceapi.sarathymotors.in',
   
   // apiUrl: 'https://sarathyserviceapi.trackbox.live/api',
-  // FilePath: 'https://sarathyserviceapi.trackbox.live/api',
+  // FilePath: 'https://sarathyserviceapi.trackbox.live',
 
   // apiUrl: 'https://ktmserviceapi.sarathymotors.in/api',
   // FilePath: 'https://ktmserviceapi.sarathymotors.in',
