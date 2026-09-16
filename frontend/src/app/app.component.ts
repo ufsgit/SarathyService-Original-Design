18d60179a7d13b11add6eaad94da264c1965eb05
NANDAN BABU <nandanbabu2003@gmail.com>
2026-09-16 17:45:25 +0530

    Loading black screen issue  FIXED
     CHANGED THE BLACK SCRREN INTO WHITE WITH SARATHY LOGO  SPINNER

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,16 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, signal } from '@angular/core';
+import { RouterOutlet, Router, Event as RouterEvent, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
 import { ApiService } from './core/services/api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor(private api: ApiService) {}
+  isRouteLoading = signal<boolean>(false);
+
+  constructor(private api: ApiService, private router: Router) {
+    this.router.events.subscribe((event: RouterEvent) => {
+      if (event instanceof NavigationStart) {
+        this.isRouteLoading.set(true);
+      } else if (
+        event instanceof NavigationEnd ||
+        event instanceof NavigationCancel ||
+        event instanceof NavigationError
+      ) {
+        this.isRouteLoading.set(false);
+      }
+    });
+  }
 
   ngOnInit() {
     // Brand color is initialized globally by BrandService via APP_INITIALIZER
